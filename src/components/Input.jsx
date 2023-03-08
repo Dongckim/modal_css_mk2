def4bd7c2290bc40531e6882d548d44fff26d933
dongckim <dck.alx@gmail.com>
2023-03-08 17:42:52 +0900

    using useRef instead useState - refactoring

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
+import BasicButton from "./BasicButton";
 
+const Input = () => {
+    const titleRef = useRef('')
+    const priceRef = useRef('')
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        alert(`name : ${ titleRef.current.value },  price : ${ priceRef.current.value.replaceAll(',','') }`)
+  };
 
-const Input = ({ titleRef }) => {
-    console.log(titleRef)
+const onChangeHandler = ()=>{
+    let str = priceRef.current.value
+    let newstr = Number(str.replaceAll(',',''))
+    if(isNaN(newstr)){
+        priceRef.current.value = 0
+    }else {
+      const formatValue = newstr.toLocaleString('ko-KR');
+      priceRef.current.value = formatValue
+    }
+};
+
     return(
-        <StInput/>
+        <form onSubmit={onSubmitHandler}>
+            이름 : <StInput ref = {titleRef} /> &nbsp;
+            가격 : <StInput ref = {priceRef} onChange={onChangeHandler} maxLength={16}/> &nbsp;
+            <BasicButton NormalSmall> 추가 </BasicButton>
+        </form>
     )
 }
 
@@ -14,5 +35,4 @@ const StInput = styled.input`
     height: 40px;
     border-radius: 8px;
 `
-
 export default Input;
